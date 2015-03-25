02a75e0ed65e77c54159b4b2eac5270834e17abe
gtg092x <mdrake@mediadrake.com>
2015-03-25 10:15:50 -0700

    Added mocha tests

--- tests/unit/numbersToTIWords.js
+++ tests/unit/numbersToTIWords.js
@@ -1,42 +1,63 @@
 var numbers = require('../../lib/numbersToTIWords');
+var assert = require("assert")
+var fs = require('fs');
+var PATH = require('path');
+var should = require('should');
 
-var tests = module.exports = {
-	tiTest: function () {
-		var input = '228';
-		console.log('input', input.split(''));
-		numbers.getAllCombinations(input.split(''), console.log);
-	},
-	wordTest: function () {
-		var input = [
-			'AAU',
-			'AAV',
-			'ABT',
-			'ABU',
-			'ABV',
-			'ACT',
-			'ACU',
-			'ACV',
-			'BAT',
-			'BAU',
-			'BAV',
-			'BBT',
-			'BBU',
-			'BBV',
-			'BCT',
-			'BCU',
-			'BCV',
-			'CAT',
-			'CAU',
-			'CAV',
-			'CBT',
-			'CBU',
-			'CBV',
-			'CCT',
-			'CCU',
-			'CCV'
-		];
-		numbers.getAllWords(input, console.log);
-	}
-}
+describe('Numbers to TI Words', function(){
+	describe('#getAllCombinations()', function(){
+		it('should get all combinations without error', function(done){
+			var input = '228';
+			numbers.getAllCombinations(input.split(''), function(err, combinations){
+				should(err).be.exactly(null);
+				done();
+			});
+		});
+		it('should get second level arrays that match input dimensions', function(done){
+			var input = '228';
+			numbers.getAllCombinations(input.split(''), function(err, combinations){
+				combinations.should.be.Array;
+				(combinations.length).should.be.above(0);
+				(combinations[0]).should.be.String;
+				(input.length).should.be.exactly(combinations[0].length);
+				done();
+			});
+		});
+		it('should ignore numbers that do not have mapped letters', function(done){
+			var input = '2281';
+			numbers.getAllCombinations(input.split(''), function(err, combinations){
+				should(err).be.exactly(null);
+				(combinations.length).should.be.above(0);
+				(input.replace(/1/g,'').length).should.be.exactly(combinations[0].length);
+				done();
+			});
+		})
+	});
+	describe('#getAllWords()', function(){
+		var words;
+		var err;
+		before(function(done){
+			var mockData = require('./mocks/word-combinations');
+			numbers.getAllWords(mockData, function(_err, _words){
+				words = _words;
+				err = _err;
+				done();
+			});
+		});
+		it('should not have an error', function(done) {
+			should(err).be.exactly(null);
+			done();
+		});
+		it('should find cat in mock', function(done){
+				var cat = words.filter(function(word){
+					return word === 'CAT';
+				});
 
-tests.wordTest();
+				should(cat).not.be.exactly(null);
+
+				done();
+
+		});
+	});
+});
+
